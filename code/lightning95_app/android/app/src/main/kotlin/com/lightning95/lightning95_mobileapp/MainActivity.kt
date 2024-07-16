83473d3ff236c7d4652579ae24f5d856ca277bdd
Yusuf Koç <71135692+yufuskoc@users.noreply.github.com>
2024-07-16 08:14:07 +0300

    Add files via upload

---
+++ code/lightning95_app/android/app/src/main/kotlin/com/lightning95/lightning95_mobileapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lightning95.lightning95_mobileapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ code/lightning95_app/lib/firebase_options.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+
+const FirebaseOptions firebaseOptions = FirebaseOptions(
+  apiKey: 'AIzaSyAdE0RNGFvmiaN-ghit-DsqkfWxxPil9tw',
+  appId: '1:164528425660:web:a9737a755b9ba7ccfa9eb9',
+  messagingSenderId: '164528425660',
+  projectId: 'lightning95-selfparking',
+  storageBucket: 'lightning95-selfparking.appspot.com',
+  databaseURL: 'https://lightning95-selfparking-default-rtdb.europe-west1.firebasedatabase.app',
+);
